5cf69b04af9c1a700b4b28f696ca31e94be85ea4
Solera <msolera@mobydigital.com>
2021-10-07 14:46:36 -0300

    Probando Commit

--- Models/Student.php
+++ Models/Student.php
@@ -6,6 +6,7 @@
     class Student extends Person
     {
         private $recordId;
+        private $probando;
 
         public function getRecordId()
         {
@@ -15,6 +16,26 @@
         public function setRecordId($recordId)
         {
             $this->recordId = $recordId;
+        }
+
+        /**
+         * Get the value of probando
+         */ 
+        public function getProbando()
+        {
+                return $this->probando;
+        }
+
+        /**
+         * Set the value of probando
+         *
+         * @return  self
+         */ 
+        public function setProbando($probando)
+        {
+                $this->probando = $probando;
+
+                return $this;
         }
     }
 ?>
