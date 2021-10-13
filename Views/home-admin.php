14b7a399b09e67fb8f1bc9e405182d5681b8382b
Solera <msolera@mobydigital.com>
2021-10-13 20:34:13 -0300

    Se crearon los Home Student/Admin

---
+++ Views/home-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+/// Aca tiene que ir el session
+
+?>
+<center>
+<h1> Home Admin </h1>
+
+<a class="nav-link" href="list-form.php">Manejo Empresas</a>
+<a class="nav-link" href="list-form.php">Cargar Propuesta Laboral</a>
+<a class="nav-link" href="list-form.php">Propuestas Laborales</a>
+<a class="nav-link" href="list-form.php">Crear Nuevos Usuarios</a>
+
+</center>
---
+++ Views/home-student.php
@@ -0,0 +1,14 @@
+<?php
+
+/// Aca tiene que ir el session
+
+?>
+<center>
+<h1> Home Student </h1>
+
+<a class="nav-link" href="list-form.php">Consulta Listado Empresas</a>
+<a class="nav-link" href="list-form.php">Consulta Propuestas Laborales</a>
+<a class="nav-link" href="list-form.php">Informacion y Estado Academico</a>
+<a class="nav-link" href="list-form.php">Consulta Historial</a>
+
+</center>
