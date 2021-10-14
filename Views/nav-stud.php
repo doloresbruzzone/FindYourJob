412e89c7d9f37606b71def1f486e0f173af61f98
doloresbruzzone <dolobruzzone@gmail.com>
2021-10-13 23:38:01 -0300

    navs

---
+++ Views/nav-stud.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     <span class="navbar-text">
+          <strong>FindYourJob - StudentUTN</strong>
+     </span>
+     <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowAddView">Companies List</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/ShowListView">Logout</a>
+          </li>          
+     </ul>
+</nav>
