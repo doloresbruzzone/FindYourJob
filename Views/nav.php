a042af454ba39bb1155ab43bb3f3e63a86330cee
Solera <msolera@mobydigital.com>
2021-10-14 13:01:52 -0300

    Modificacion del nav

--- Views/nav.php
+++ Views/nav.php
@@ -3,6 +3,7 @@
           <strong>Framework</strong>
      </span>
      <ul class="navbar-nav ml-auto">
+
           <?php if(isset($_SESSION['admin'])) { ?>
           <li class="nav-item">
                <a class="nav-link" href="<?php echo FRONT_ROOT."Company/ShowViewsAdd";?>" >Agregar Empresas</a>
@@ -10,6 +11,29 @@
           <li class="nav-item">
                <a class="nav-link" href="<?php echo FRONT_ROOT."Company/ShowListViewStudent";?>" >Listar Empresas</a>
           </li> 
-          <?php } ?>         
+          <li class="nav-item">
+          <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/LogOut">Logout</a>
+          </li> 
+          <li class="nav-item">
+          <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/LogOut">2</a>
+          </li> 
+          <li class="nav-item">
+          <a class="nav-link" href="<?php echo FRONT_ROOT ?>Company/LogOut">3</a>
+          </li> 
+
+          <?php } ?>   
+          
+          <?php if(isset($_SESSION['student'])) { ?>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT."Company/ShowViewsAdd";?>" >Agregar Empresas</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Student/LogOut">Logout</a>
+          </li> 
+          <?php } ?>
+          
+
+
+
      </ul>
 </nav>
