cf9c1c6d8eed89733282640e95c89a3efdcc4f1d
pzaCristian <66428958+pzaCristian@users.noreply.github.com>
2021-10-15 16:23:24 -0300

    Update addCompany.php

--- Views/addCompany.php
+++ Views/addCompany.php
@@ -11,17 +11,17 @@
                          <div class="col-lg-3">
                               <div class="form-group">
                                  <label for="">Name</label>
-                                 <input type="text" name="name" required>
-                                 <label for="">Year Foundation</label>
-                                 <input type="number" name="year" min="0" required>
+                                 <input type="text" name="name" placeholder="Name"required>
+                                 <label for="">Year Foundation</label><br>
+                                 <input type="number" name="year" min="0" max="2021" placeholder="aaaa" required><br>
                                  <label for="">City</label>
-                                 <input type="text" name="city" required>
+                                 <input type="text" name="city" placeholder="City" required>
                                  <label for="">Description</label>
-                                 <input type="text" name="description" minlength="10" maxlength="1000" required>
+                                 <input type="text" name="description" minlength="10" maxlength="1000" placeholder="Description" required>
                                  <label for="">Email</label>
-                                 <input type="email" name="email" required>
+                                 <input type="email" name="email" placeholder="email"required>
                                  <label for="">Phone Number</label>
-                                 <input type="text" name="phone" required>
+                                 <input type="text" name="phone" placeholder="Phone Number" required>
                                  <label for="">Photo</label>
                                  <input type="file" name ="logo" value ="<?php/* echo IMGCOOL_PATH.$product->getPhoto();*/?>"> 
                               </div>
