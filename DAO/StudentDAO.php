1385b42675fdf709b2a55da67953be39accaf3d0
pzaCristian <66428958+pzaCristian@users.noreply.github.com>
2021-10-21 01:03:10 -0300

    Update StudentDAO.php

--- DAO/StudentDAO.php
+++ DAO/StudentDAO.php
@@ -3,10 +3,89 @@
 
     use DAO\IStudentDAO as IStudentDAO;
     use Models\Student as Student;
+    use DAO\Connection as Connection;
     
     class StudentDAO implements IStudentDAO
     {
         private $studentList = array();
+        private $connection;
+
+
+        public function Add($student)
+        {
+            
+          $sql = "INSERT INTO students (firstName, lastName, dni, fileNumber, gender, birthDate, phoneNumber, active)
+                     VALUES (:firstName, :lastName, :dni, :fileNumber, :gender, :birthDate, :phoneNumber, :active);";
+            $parameters["firstName"]=$student->getFirstName();
+            $parameters['lastName']=$student->getLastName();
+            $parameters['dni']=$student->getDni();
+            $parameters['gender']=$student->getGender();
+            $parameters['birthDate']=$student->getBirthDate();
+            $parameters['phoneNumber']=$student->getPhoneNumber();
+            $parameters['active']=true;
+
+            
+            try {
+                $this->connection= Connection::getInstance();
+                return $this->connection->executeNonQuery($sql, $parameters);
+            } catch (\PDOException $ex) {
+                throw $ex;
+            }
+        }
+
+        public function Delete($idToDelete){
+
+            $sql = "DELETE FROM students WHERE studentId=:studentId";
+            $parameters['studentId']=$idToDelete;
+            try{
+                $this->connection = Connection::getInstance();
+                return $this->connection->executeNonQuery($sql, $parameters);
+            }catch(\PDOException $exeption){
+                throw $exeption;
+            }
+
+        }
+
+        public function Update($student, $toFind){
+            $sql = "UPDATE students set careerId=:careerId, firstName=:firstName, lastName=:lastName, dni=:dni, fileNumber=:fileNumber, 
+                     gender=:gender, birthDate=:birthDate, email=:email, phoneNumber=:phoneNumber WHERE studentId= '$toFind';";
+
+            $parameters["firstName"]=$student->getFirstName();
+            $parameters['lastName']=$student->getLastName();
+            $parameters['dni']=$student->getDni();
+            $parameters['gender']=$student->getGender();
+            $parameters['birthDate']=$student->getBirthDate();
+            $parameters['phoneNumber']=$student->getPhoneNumber();
+            $parameters['active']=$student->getActive();
+
+            try{
+                $this->connection=Connection::getInstance();
+                return $this->connection->executeNonQuery($sql, $parameters);
+            }catch(\PDOException $exeption){
+                throw $exeption;
+            }
+                     
+        }
+
+        /*public function Search($email){
+            $sql = "SELECT * FROM students WHERE email=:email";
+            $parameters['email']=$email;
+            try{
+                $this->connection = Connection::getInstance();
+                $result=$this->connection->execute($sql, $parameters);
+
+            }catch(\PDOException $exeption){
+                throw $exeption;
+            }
+
+            if(!empty($result)){
+                return $this->mapear($result);
+            }else{
+                return false;
+            }
+
+        }*/
+
         
         public function GetAll()
         {
