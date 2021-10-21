cd2e68f5143cb7139fe328a796d104ac4ee894d2
pzaCristian <66428958+pzaCristian@users.noreply.github.com>
2021-10-20 22:22:35 -0300

    Update home-admin.php

--- Views/home-admin.php
+++ Views/home-admin.php
@@ -1,16 +1,31 @@
 <?php
-
-/// Aca tiene que ir el session
-
+ require_once('nav.php');
 ?>
 
-<h1> Home Admin </h1>
+<body>
+<header class="text-center">
+    <br><br>
+<img src="<?php echo IMG_PATH ?>homeAdmin.png" width="200" height="" alt=""/>
+       </header>
+    <!-- Header-->
+    <br><br>
+    <header class="d-flex align-items-center justify-content-center height-50">   
+                    
+    
+        <div class="container-menu px-8 px-lg-1 text-center ">
+        <!-- <div class="view-container"> -->
+     
+        <h1 p class="text-primary" class="mb-1">You are Welcome</h1>
+        
+            <h2> Administrator </h2>
+            <h5 class="mb-5"><em>Please choose one of the next actions</em></h5>
+            <a class="btn btn-success btn-x2" href="<?php echo FRONT_ROOT ?>Company/RedirectAddForm">Add Company</a>
+           <!-- <a class="btn btn-success btn-x2" href="<?php echo FRONT_ROOT ?>Company/RedirectDeleteForm">Delete Company</a>  -->
+            <a class="btn btn-success btn-x2" href="<?php echo FRONT_ROOT ?>Company/ShowListViewAdmin">Company Management</a>
+             <!--<a class="btn btn-primary btn-xl" href="#">Lista de Propuestas</a>-->
+            <a class="btn btn-success btn-xl" href="<?php echo FRONT_ROOT ?>Student/ShowListView">Students List</a>
+            <a class="btn btn-success btn-xl" href="<?php echo FRONT_ROOT ?>Company/LogOut">LogOut</a>
+        </div>
+    </header>
 
-<center>
-
-<a class="nav-link" href="list-form.php">Manejo Empresas</a>
-<a class="nav-link" href="list-form.php">Cargar Propuesta Laboral</a>
-<a class="nav-link" href="list-form.php">Propuestas Laborales</a>
-<a class="nav-link" href="list-form.php">Crear Nuevos Usuarios</a>
-
-</center>
+</body>
